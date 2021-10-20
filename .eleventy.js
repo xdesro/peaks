e2e11b5099774cda7d22b67b905b35bfbc6aab1a
Henry Desroches <yo@henry.codes>
2021-10-20 14:51:49 -0600

    🏷 - Add meta tagging

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/fonts');
   // eleventyConfig.addPassthroughCopy('src/functions');
   eleventyConfig.addPassthroughCopy('src/img');
-  // eleventyConfig.addPassthroughCopy('src/meta');
+  eleventyConfig.addPassthroughCopy('src/meta');
 
   eleventyConfig.setWatchThrottleWaitTime(200);
   eleventyConfig.addWatchTarget('./dist/css');
