b62e13b1636fbb2de5b469c72ccc21bd056b1ed2
Henry Desroches <yo@henry.codes>
2021-10-21 10:03:48 -0600

    💻 - Javascript is a sin

--- src/_includes/Footer.vue
+++ src/_includes/Footer.vue
@@ -7,7 +7,8 @@
         <a href="https://github.com/xdesro/peaks">the source code is here</a>.
       </p>
       <p>
-        Uses <a href="https://www.11ty.dev/">Eleventy</a> for templating, and good ol' HTML and CSS. Fonts in use are
+        Uses <a href="https://www.11ty.dev/">Eleventy</a> for templating, and good ol' HTML and CSS. There's no
+        JavaScript because I believe it is a sin. Fonts in use are
         <a href="https://bretagnebretagne.fr/font/self-modern-regular/">Self Modern</a> and
         <a href="https://catalog.monotype.com/family/linotype/neue-haas-grotesk-display">Neue Haas Grotesk</a>.
       </p>
